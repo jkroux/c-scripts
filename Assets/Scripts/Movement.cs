8dd493001e55f62e5c90a7dc696a716e15432c4a
mjulstro <mjulstro@macalester.edu>
2017-02-15 13:51:03 -0600

    Files for running prototype of game on Unity
    These should be all the files necessary? I'm not 100% sure that they will be, though, because I don't know if your libraries contain the same things as mine, or which parts of the contents of my "First Version" folder are necessary parts of the project and which are parts of Unity. If it doesn't work, we can try uploading different things.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;
+    public int artInRoom;
+    private int artCollected;
+
+	// Use this for initialization
+	void Start () {
+        artCollected = 0;
+	}
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate () {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Actually move the player's icon
+        transform.Translate(movement * speed);
+    }
+
+    //OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+    void OnTriggerEnter2D(Collider2D other) {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("Pickup")) {
+            other.gameObject.SetActive(false);
+            artCollected++;
+        }
+        else if (other.gameObject.CompareTag("Door")) {
+            if (artCollected == artInRoom) {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}

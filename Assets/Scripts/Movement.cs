c5a5c1427c47e10322940f1ca3909d835c1818a1
jkroux <jroux@macalester.edu>
2017-03-01 13:22:01 -0600

    TripWire
    TripWire

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,6 +9,8 @@ public class Movement : MonoBehaviour {
     private int artCollected;
 	static public bool cardObtained;
 	public GameObject door;
+	public GameObject TripWire;
+	public GameObject Gaurd;
 
 	// Use this for initialization
 	void Start () {
@@ -46,27 +48,43 @@ public class Movement : MonoBehaviour {
         transform.Translate(movement * speed);
     }
 
-    //OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
-    void OnTriggerStay2D(Collider2D other) {
-        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
-		if (other.gameObject.CompareTag("Pickup") && Input.GetKeyDown(KeyCode.E)) {
-            other.gameObject.SetActive(false);
-            artCollected++;
-        }
-        if (other.gameObject.CompareTag("Door")) {
-            if (artCollected == artInRoom) {
-                gameObject.SetActive(false);
+	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+	void OnTriggerStay2D(Collider2D other)
+	{
+		//Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+		if (other.gameObject.CompareTag("Pickup") && Input.GetKeyDown(KeyCode.E))
+		{
+			other.gameObject.SetActive(false);
+			artCollected++;
+		}
+		if (other.gameObject.CompareTag("Door"))
+		{
+			if (artCollected == artInRoom)
+			{
+				gameObject.SetActive(false);
 			}
 		}
-		if (other.gameObject.CompareTag("KeyCard") && Input.GetKeyDown(KeyCode.E)){
-			cardObtained=true;
+		if (other.gameObject.CompareTag("KeyCard") && Input.GetKeyDown(KeyCode.E))
+		{
+			cardObtained = true;
 			print("card obtained");
 			other.gameObject.SetActive(false);
 			// code taken in part from unity 3d https://forum.unity3d.com/threads/how-do-you-change-a-color-in-spriterenderer.211003/
-			   SpriteRenderer renderer = (SpriteRenderer) door.GetComponent<Renderer>();
-			   renderer.color = new Color32(17,161,54,255);
+			SpriteRenderer renderer = (SpriteRenderer)door.GetComponent<Renderer>();
+			renderer.color = new Color32(17, 161, 54, 255);
 			BoxCollider2D comp = door.GetComponent("BoxCollider2D") as BoxCollider2D;
 			comp.enabled = false;
+		}
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		print("register");
+		if (other.gameObject.CompareTag("Triggered"))
+		{
+			SpriteRenderer render = (SpriteRenderer)TripWire.gameObject.GetComponent<Renderer>();
+			render.color = new Color(0f, 0f, 0f, 1f);
+			Ai_movement comp = Gaurd.GetComponent("Ai_movement") as Ai_movement;
+			comp.speed = 3f;
+			print("got em");
 		}
 			
     }
---
+++ Assets/Scripts/Tripwire.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tripwire : MonoBehaviour {
+	public bool Triped=false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Triped==true){
+		}
+		
+	}
+	void OnTriggerEnter2D(Collider2D other){
+	}
+}

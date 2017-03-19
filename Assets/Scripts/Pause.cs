e4b3c8ed82a5e67a2d2be145d5190571baecb034
mjulstro <mjulstro@macalester.edu>
2017-03-18 23:06:19 -0500

    In-Progress Pause Script
    The "Pause" script, a component of the PauseHandler object, now includes
    code to disable both player and guard movement (including when there are
    multiple guards) when the player picks up a pickup. THIS CODE DOES NOT
    WORK, but it shouldn't affect anything else we're working on. If it
    does, just disable the PauseHandler object, since it doesn't do anything
    else.
    
    (The reason this code doesn't work, as far as I can tell, is that the
    PauseHandler object has no way of determining when the Player object has
    picked up a pickup, or is within the Collider2D of an object tagged
    "Pickup". I'm trying to fix this by putting this code in a new public
    method, which can be called from the Movement script when an object is
    picked up; the problem there is getting the Player object to identify
    the PauseHandler object and "know" that this method can be called.
    Again, if you guys don't want to mess with this, you don't have to; just
    disable the PauseHandler in the hierarchy. These notes are mostly for
    myself tomorrow morning.)

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour {
 
+    private GameObject[] guardList;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	// Pauses the game when the player picks up an object, so the player has time to look at the
+    // popup window describing the object.
+	void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                guardList = GameObject.FindGameObjectsWithTag("Guard");
+                foreach (GameObject currentGuard in guardList) {
+                    (currentGuard.GetComponent("Ai_movement") as MonoBehaviour).enabled = false;
+                }
+
+                (GameObject.FindWithTag("Player").GetComponent("Movement") as MonoBehaviour).enabled = false;
+            }
+        }
+    }
 }

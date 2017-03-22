6aa94680a800e56c985841ad395c443da9aa0c23
xcwang <534059>
2017-03-22 12:52:34 -0500

    Clean the useless code + let the player stop when being caught
    *Clean/comment out the useless code
    * let the player stop moving when it is being caught by the guard
    instead of disappearing
    * some code of chasing the player
    
    There is still a bug in the chasePlayer method, so I currently comment
    all the code about this method out. Therefore, now the guard will stop
    moving when the player hit the vision cone, but the player can still
    move. This bug will be fixed when I uncomment the code about chasing
    the player.

--- Assets/Scripts/Ai_movement.cs
+++ Assets/Scripts/Ai_movement.cs
@@ -8,13 +8,16 @@ public class Ai_movement : MonoBehaviour
 {
 	public int time = 0;
 	public float speed = 1;
+	public float chasingSpeed = 1;
+	public GameObject player;
+
 	private Vector2 movement;
-	private Transform target;
-	private bool chase;
-	int xDir, yDir;
-	Movement store;
-	float hp;
-	bool canSee;
+	private bool chase;	
+	private bool canSee;
+	private float offsetX;
+	private float offsetY;
+	private float distance;
+	private bool caught;
 	LayerMask mask;
 	public float visionAngle;
 	public float stepCount;
@@ -26,10 +29,7 @@ public class Ai_movement : MonoBehaviour
 		mask = 1 << 2;
 		mask = ~mask;
 		movement = new Vector2(1, 0);
-		target = GameObject.FindGameObjectWithTag("Player").transform;
 		chase = false;
-		xDir = 0;
-		yDir = 0;
 		canSee = true;
 		timer=0;
 		List<Vector2> angleMeasure =Angle ();
@@ -37,26 +37,34 @@ public class Ai_movement : MonoBehaviour
 	}
 
 	// Update is called once per frame
-	void Update()
-	{
-		if (chase == false)
-		{
+	void Update(){
+		if (chase == false){
 			time++;
-			if ((time == 75) || (time == 150) || (time == 225))
-			{
+			if ((time == 75) || (time == 150) || (time == 225)){
 				transform.Rotate(0, 0, 90);
 			}
-			else if (time >= 300)
-			{
+			else if (time >= 300){
 				transform.Rotate(0, 0, 90);
 				time = 0;
 			}
 			transform.Translate(movement * speed);
 		}
 		else {
-			Movement.playerHp--;
+			offsetX = Mathf.Abs (transform.position.x - player.transform.position.x);
+			offsetY = Mathf.Abs (transform.position.y - player.transform.position.y);
+			distance = Mathf.Sqrt (Mathf.Pow (offsetX,2) + Mathf.Pow (offsetY,2));
+			if (distance < 0.5) {
+				SpriteRenderer render2 = (SpriteRenderer)player.GetComponent<Renderer> ();
+				render2.color = new Color (.5f, .2f, 1f, 1f);
+				caught = true;
+				print ("you have been caught");
+			}
+			else {
+		//		ChasePlayer ();    //There is a bug in the ChasePlayer method. I will uncomment this when I fix this bug
 		}
+	  }
 	}
+
 	 public List<Vector2> Angle(){
 		float stepAngleSize = visionAngle / stepCount;
 		List<Vector2> viewPoint = new List<Vector2> ();
@@ -68,11 +76,12 @@ public class Ai_movement : MonoBehaviour
 		return viewPoint;
 	}
 
+	//???
 	public void visionCircle(){
 		List<Vector2> dirVector = Angle ();
 		for (int i=0; i<=dirVector.Count;i++){
 			RaycastHit2D hit = Physics2D.Raycast (transform.position, dirVector[i],1, mask);
-			if (hit.collider.tag=="Player") {
+			if (hit.collider.tag=="Player") {  
 				canSee = true;
 				//chase = true;
 				print (canSee);
@@ -81,10 +90,8 @@ public class Ai_movement : MonoBehaviour
 				canSee = false;
 	
 		}
+	  }
 	}
-	}
-		
-
 
 	void FixedUpdate(){
 		List<Vector2> dirVector = Angle ();
@@ -94,17 +101,17 @@ public class Ai_movement : MonoBehaviour
 				if (hit.collider.tag == "Player") {
 					chase = true;
 					timer = 0;
-				}
-				else if (hit.collider.tag != "Player"){
+				} else if (hit.collider.tag != "Player") {
 					timer++;
-					if (timer>=900*6){
+					if (timer >= 900 * 6) {  //????
 						chase = false;
+					}
+				}
 			}
 		}
 	}
-		}
-	}
 
+	//do we still need this?
 	void OnTriggerStay2D(Collider2D other){
 
 		if (other.gameObject.CompareTag("Player")&& canSee==true){
@@ -112,7 +119,8 @@ public class Ai_movement : MonoBehaviour
 			other.gameObject.GetComponent<Movement>();
 		}
 	}
-	void OnTriggerEnter2d(Collider2D other){
+
+//	void OnTriggerEnter2d(Collider2D other){
 //		if (other.gameObject.CompareTag ("Walls")) {
 //			Vector2 wall = other.gameObject.transform.position;
 //			float xWall = wall.x;
@@ -120,5 +128,15 @@ public class Ai_movement : MonoBehaviour
 //			BoxCollider2D collider = GetComponent<BoxCollider2D> ();
 //			//Vector2 boxBox=collider.size;
 //		}
+//	}
+
+	void ChasePlayer(){
+		Vector2 unitVector = new Vector2 (offsetX / distance, offsetY / distance);
+		transform.Translate (unitVector * chasingSpeed);
 	}
+
+	public bool getCaught(){
+		return (caught);
+	}
+
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,52 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-	public static float playerHp;
     public float speed;
     public int artInRoom;
     private int artCollected;
 	static public bool cardObtained;
 	public GameObject door;
-	public GameObject TripWire;
-	public GameObject Gaurd;
+	public GameObject Guard;
 
 
 	// Use this for initialization
 	void Start () {
-		playerHp=1;
 		Rigidbody2D playerBody = (Rigidbody2D) gameObject.GetComponent<Rigidbody2D> ();
 		playerBody.freezeRotation = true;
         artCollected = 0;
 		cardObtained=false;
 		print ("press E to pick up objects");
 	}
-
-	public bool cardObtainedget(){
-		return(cardObtained);
-	}
-
-	void Update(){
-		if (playerHp==0){
-			SpriteRenderer render2 = (SpriteRenderer) gameObject.GetComponent<Renderer>();
-			render2.color = new Color(.5f, .2f, 1f, 1f);
-			gameObject.SetActive(false);
-			print("you have been caught");
-		}
-	}
-
+		
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate () {
-        //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis("Horizontal");
-
-        //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis("Vertical");
-
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-
-        //Actually move the player's icon
-        transform.Translate(movement * speed);
+		Ai_movement guardMovement = Guard.GetComponent<Ai_movement>();
+		bool caught = guardMovement.getCaught();
+		if (!caught) {
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+			Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+			transform.Translate (movement * speed);
+		}
     }
 
 	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
@@ -74,12 +55,14 @@ public class Movement : MonoBehaviour {
 			other.gameObject.SetActive(false);
 			// code taken in part from unity 3d https://forum.unity3d.com/threads/how-do-you-change-a-color-in-spriterenderer.211003/
 			SpriteRenderer renderer = (SpriteRenderer)door.GetComponent<Renderer>();
+//<<<<<<< Updated upstream
 			renderer.color = new Color32(17, 161, 54, 255);
 			BoxCollider2D comp = door.GetComponent("BoxCollider2D") as BoxCollider2D;
 			comp.enabled = false;
 		}
 	}
-	void OnTriggerEnter2D(Collider2D other){
+
+//	void OnTriggerEnter2D(Collider2D other){
 //		print("register");
 //		if (other.gameObject.CompareTag("Triggered"))
 //		{
@@ -89,6 +72,24 @@ public class Movement : MonoBehaviour {
 //			comp.speed = 3f;
 //			print("got em");
 //		}
+//=======
+//			renderer.color = new Color32(17, 161, 54, 255);
+//			BoxCollider2D comp = door.GetComponent("BoxCollider2D") as BoxCollider2D;
+//			comp.enabled = false;
+//		}
+//	}
+
+//	void OnTriggerEnter2D(Collider2D other){
+//		print("register");
+//		if (other.gameObject.CompareTag("Triggered"))
+//		{
+//			SpriteRenderer render = (SpriteRenderer)TripWire.gameObject.GetComponent<Renderer>();
+//			render.color = new Color(0f, 0f, 0f, 1f);
+//			Ai_movement comp = Gaurd.GetComponent("Ai_movement") as Ai_movement;
+//			comp.speed = 3f;
+//			print("got em");
+//		}
+//>>>>>>> Stashed changes
 			
-    }
+//    }
 }

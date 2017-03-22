ba8d05f366339ffaaab14642d2f08a9c24c6e65f
jkroux <jroux@macalester.edu>
2017-03-21 19:34:22 -0500

    So the gaurd now sees in a circle need to intigrate chase mechanic into gaurd movement/sight

--- Assets/Scripts/Ai_movement.cs
+++ Assets/Scripts/Ai_movement.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 // movement to player solved using unity tutorial
 //http://answers.unity3d.com/questions/32618/changing-box-collider-size.html
 //box collider resizing.
@@ -15,9 +16,9 @@ public class Ai_movement : MonoBehaviour
 	float hp;
 	bool canSee;
 	LayerMask mask;
-
-
-	 
+	public float visionAngle;
+	public float stepCount;
+	private int timer;
 
 	// Use this for initialization
 	void Start()
@@ -30,6 +31,9 @@ public class Ai_movement : MonoBehaviour
 		xDir = 0;
 		yDir = 0;
 		canSee = true;
+		timer=0;
+		List<Vector2> angleMeasure =Angle ();
+	
 	}
 
 	// Update is called once per frame
@@ -53,18 +57,54 @@ public class Ai_movement : MonoBehaviour
 			Movement.playerHp--;
 		}
 	}
-	void FixedUpdate(){
-		RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.up,Mathf.Infinity, mask);
-		if (hit.collider.tag=="Wall") {
-			canSee = false;
-			//chase = true;
-			print (canSee);
-		} 
-		else {
-			canSee = true;
+	 public List<Vector2> Angle(){
+		float stepAngleSize = visionAngle / stepCount;
+		List<Vector2> viewPoint = new List<Vector2> ();
+		for (int i=0; i<= stepCount; i++){
+			float angle = transform.eulerAngles.y - visionAngle / 2 + stepAngleSize*i;
+			Vector2 vAnglev = new Vector2 (Mathf.Sin (angle * Mathf.Deg2Rad) ,(Mathf.Cos (angle * Mathf.Deg2Rad)));
+			viewPoint.Add (vAnglev);	
+		}
+		return viewPoint;
+	}
+
+	public void visionCircle(){
+		List<Vector2> dirVector = Angle ();
+		for (int i=0; i<=dirVector.Count;i++){
+			RaycastHit2D hit = Physics2D.Raycast (transform.position, dirVector[i],1, mask);
+			if (hit.collider.tag=="Player") {
+				canSee = true;
+				//chase = true;
+				print (canSee);
+			} 
+			else {
+				canSee = false;
+	
 		}
 	}
+	}
 		
+
+
+	void FixedUpdate(){
+		List<Vector2> dirVector = Angle ();
+		for (int i = 0; i < dirVector.Count; i++) {
+			RaycastHit2D hit = Physics2D.Raycast (transform.position, dirVector [i], 2, mask);
+			if (hit.collider != null) {
+				if (hit.collider.tag == "Player") {
+					chase = true;
+					timer = 0;
+				}
+				else if (hit.collider.tag != "Player"){
+					timer++;
+					if (timer>=900*6){
+						chase = false;
+			}
+		}
+	}
+		}
+	}
+
 	void OnTriggerStay2D(Collider2D other){
 
 		if (other.gameObject.CompareTag("Player")&& canSee==true){
@@ -73,12 +113,12 @@ public class Ai_movement : MonoBehaviour
 		}
 	}
 	void OnTriggerEnter2d(Collider2D other){
-		if (other.gameObject.CompareTag ("Walls")) {
-			Vector2 wall = other.gameObject.transform.position;
-			float xWall = wall.x;
-			float yWall = wall.y;
-			BoxCollider2D collider = GetComponent<BoxCollider2D> ();
-			Vector2 boxBox=collider.size;
-		}
+//		if (other.gameObject.CompareTag ("Walls")) {
+//			Vector2 wall = other.gameObject.transform.position;
+//			float xWall = wall.x;
+//			float yWall = wall.y;
+//			BoxCollider2D collider = GetComponent<BoxCollider2D> ();
+//			//Vector2 boxBox=collider.size;
+//		}
 	}
 }
